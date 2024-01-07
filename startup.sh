d8a0c2004bd185ea97697f46c087345a1596afa6
Ariel Tempelhof <arieltm@gmail.com>
2024-01-07 18:47:08 +0200

    add zsh-autosuggestions

--- startup.sh
+++ startup.sh
@@ -5,9 +5,10 @@ sudo apt update && sudo apt dist-upgrade -y && sudo apt install zsh vim jq tmux 
 RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sed 's/ZSH_THEME=.*/ZSH_THEME="agnoster"/g' .zshrc > .tmpzshrc && mv .tmpzshrc .zshrc
-sed 's/plugins=(.*)/plugins=(git command-not-found docker tmux)\nZSH_TMUX_AUTOSTART=true\nexport EDITOR=vim/g' .zshrc > .tmpzshrc && mv .tmpzshrc .zshrc
+sed 's/plugins=(.*)/plugins=(git command-not-found docker tmux zsh-autosuggestions)\nZSH_TMUX_AUTOSTART=true\nexport EDITOR=vim/g' .zshrc > .tmpzshrc && mv .tmpzshrc .zshrc
 
 echo "Resarting in 5..."
 sleep 5
