9d987babbdd982a7c23c29636b42b368190e2117
Ariel Tempelhof <arieltm@gmail.com>
2025-07-11 07:36:15 +0300

    make non interactive

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt dist-upgrade -y && sudo apt install zsh vim jq tmux git aria2 net-tools python3-pip pv build-essential pigz unzip curl -y
 
-RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 git clone https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -15,6 +15,8 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vi
 echo "set number" >> ~/.vim_runtime/my_configs.vim
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+sudo chsh -s "$(which zsh)" "$USER"
 
 echo "Resarting in 5..."
 sleep 5
