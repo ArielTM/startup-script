56a7aae9ac370284fb0fb815ef0493e67c22d112
ArielTM <arieltm@gmail.com>
2026-02-03 16:27:35 +1100

    fix: add nvm configuration to zsh

--- startup.sh
+++ startup.sh
@@ -16,7 +16,7 @@ echo "set number" >> ~/.vim_runtime/my_configs.vim
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash && echo -e '\nexport NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh"\n[ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion"' >> ~/.zshrc
 
 sudo chsh -s "$(which zsh)" "$USER"
 
